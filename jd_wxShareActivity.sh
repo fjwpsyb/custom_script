f55c3df5abce34a3aeb1db73ad5d5e91d3651965
fjwpsyb <350381204@qq.com>
2023-02-23 11:33:17 +0800

    新增分享有礼多车头自动脚本

---
+++ jd_wxShareActivity.sh
@@ -0,0 +1,311 @@
+#!/bin/bash
+:<<!
+/***
+* @Description 分享有礼 custom_script/jd_wxShareActivity.sh
+* @author Doraemon
+* @data 2023-01-09 15:26:09
+*/
+!
+
+script=KingRan_KR/jd_wxShareActivity.js
+sleepValue=30
+
+echo '开始执行'
+task $script desi JD_COOKIE 2 100-1 now
+echo '2 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 3 100-1 now
+echo '3 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 4 100-1 now
+echo '4 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 5 100-1 now
+echo '5 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 6 100-1 now
+echo '6 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 7 100-1 now
+echo '7 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 8 100-1 now
+echo '8 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 9 100-1 now
+echo '9 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 10 100-1 now
+echo '10 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 11 100-1 now
+echo '11 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 12 100-1 now
+echo '12 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 13 100-1 now
+echo '13 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 14 100-1 now
+echo '14 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 15 100-1 now
+echo '15 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 16 100-1 now
+echo '16 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 17 100-1 now
+echo '17 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 18 100-1 now
+echo '18 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 19 100-1 now
+echo '19 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 20 100-1 now
+echo '20 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 21 100-1 now
+echo '21 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 22 100-1 now
+echo '22 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 23 100-1 now
+echo '23 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 24 100-1 now
+echo '24 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 25 100-1 now
+echo '25 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 26 100-1 now
+echo '26 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 27 100-1 now
+echo '27 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 28 100-1 now
+echo '28 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 29 100-1 now
+echo '29 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 30 100-1 now
+echo '30 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 31 100-1 now
+echo '31 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 32 100-1 now
+echo '32 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 33 100-1 now
+echo '33 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 34 100-1 now
+echo '34 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 35 100-1 now
+echo '35 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 36 100-1 now
+echo '36 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 37 100-1 now
+echo '37 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 38 100-1 now
+echo '38 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 39 100-1 now
+echo '39 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 40 100-1 now
+echo '40 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 41 100-1 now
+echo '41 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 42 100-1 now
+echo '42 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 43 100-1 now
+echo '43 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 44 100-1 now
+echo '44 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 45 100-1 now
+echo '45 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 46 100-1 now
+echo '46 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 47 100-1 now
+echo '47 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 48 100-1 now
+echo '48 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 49 100-1 now
+echo '49 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 50 100-1 now
+echo '50 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 51 100-1 now
+echo '51 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 52 100-1 now
+echo '52 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 53 100-1 now
+echo '53 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 54 100-1 now
+echo '54 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 55 100-1 now
+echo '55 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 56 100-1 now
+echo '56 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 57 100-1 now
+echo '57 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 58 100-1 now
+echo '58 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 59 100-1 now
+echo '59 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 60 100-1 now
+echo '60 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 61 100-1 now
+echo '61 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 62 100-1 now
+echo '62 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 63 100-1 now
+echo '63 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 64 100-1 now
+echo '64 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 65 100-1 now
+echo '65 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 66 100-1 now
+echo '66 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 67 100-1 now
+echo '67 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 68 100-1 now
+echo '68 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 69 100-1 now
+echo '69 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 70 100-1 now
+echo '70 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 71 100-1 now
+echo '71 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 72 100-1 now
+echo '72 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 73 100-1 now
+echo '73 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 74 100-1 now
+echo '74 100-1 success'
+sleep $sleepValue
+
+task $script desi JD_COOKIE 75 100-1 now
+echo '75 100-1 success'
+sleep $sleepValue
+
+
+echo '执行结束'
