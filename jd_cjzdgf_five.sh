3c17c1d1e9e662608dc32b96fb240feda8bfc9f6
fjwpsyb <350381204@qq.com>
2023-02-27 20:25:27 +0800

    优化脚本

--- jd_cjzdgf_five.sh
+++ jd_cjzdgf_five.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 script=feverrun_my_scripts/jd_cjhydz_wxTeam.js
 sleepValue=10
+
 twoGroup='2 1 10-100'
 threeGroup='3 30-100'
 fourGroup='4 49-100'
 fiveGroup='5 69-100'
+
 echo '组队瓜分开始执行'
+
 task $script desi JD_COOKIE $twoGroup now
 echo $twoGroup 'success'
-sleep 10
+sleep $sleepValue
+
 task $script desi JD_COOKIE $threeGroup now
 echo $threeGroup 'success'
-sleep 10
+sleep $sleepValue
+
 task $script desi JD_COOKIE $fourGroup now
 echo $fourGroup 'success'
-sleep 10
+sleep $sleepValue
+
 task $script desi JD_COOKIE $fiveGroup now
 echo $fiveGroup 'success'
-sleep 10
+sleep $sleepValue
+
 echo '组队瓜分执行结束'
