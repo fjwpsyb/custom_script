6625f82ca44025f40c7701fc4ab041bd4f48836b
fjwpsyb <350381204@qq.com>
2023-02-27 20:22:46 +0800

    优化脚本

--- jd_cjzdgf_five.sh
+++ jd_cjzdgf_five.sh
@@ -5,17 +5,22 @@ twoGroup='2 1 10-100'
 threeGroup='3 30-100'
 fourGroup='4 49-100'
 fiveGroup='5 69-100'
+
 echo '组队瓜分开始执行'
+
 task $script desi JD_COOKIE $twoGroup now
 echo $twoGroup 'success'
-sleep $sleepValue
+sleep 10
+
 task $script desi JD_COOKIE $threeGroup now
 echo $threeGroup 'success'
-sleep $sleepValue
+sleep 10
+
 task $script desi JD_COOKIE $fourGroup now
 echo $fourGroup 'success'
-sleep $sleepValue
+sleep 10
+
 task $script desi JD_COOKIE $fiveGroup now
 echo $fiveGroup 'success'
-sleep $sleepValue
+sleep 10
 echo '组队瓜分执行结束'
