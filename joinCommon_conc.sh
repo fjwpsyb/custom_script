16cc8e7deb70982a2ea6e3cf2dab79e8ee3ad2d3
fjwpsyb <350381204@qq.com>
2023-03-03 18:02:17 +0800

    增加通用开卡并发脚本

---
+++ joinCommon_conc.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+script=HarbourJ_HarbourToulu_main/jd_joinCommon_opencard.py
+sleepValue=80
+
+one='1-10'
+two='1 11-20'
+three='1 21-30'
+four='1 31-40'
+five='1 41-50'
+six='1 51-60'
+seven='1 61-70'
+eight='1 71-80'
+nine='1 81-90'
+ten='1 91-100'
+eleven='1 101-110'
+Twelve='1 111-120'
+thirteen='1 121-130'
+fourteen='1 131-140'
+fifteen='1 141-150'
+sixteen='1 151-160'
+seventeen='1 161-170'
+eighteen='1 171-180'
+nineteen='1 181-190'
+twenty_one='1 191-200'
+twenty_two='1 201-210'
+twenty_three='1 211-220'
+twenty_four='1 221-230'
+twenty_five='1 231-240'
+
+task $script conc JD_COOKIE $one now
+echo  $one 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $two now
+echo  $two 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $three now
+echo  $three 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $four now
+echo  $four 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $five now
+echo  $five 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $six now
+echo  $six 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $seven now
+echo  $seven 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $eight now
+echo  $eight 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $nine now
+echo  $nine 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $ten now
+echo  $ten 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $eleven now
+echo  $eleven 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $Twelve now
+echo  $Twelve 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $thirteen now
+echo  $thirteen 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $fourteen now
+echo  $fourteen 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $fifteen now
+echo  $fifteen 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $sixteen now
+echo  $sixteen 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $seventeen now
+echo  $seventeen 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $eighteen now
+echo  $eighteen 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $nineteen now
+echo  $nineteen 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $twenty_one now
+echo  $twenty_one 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $twenty_two now
+echo  $twenty_two 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $twenty_three now
+echo  $twenty_three 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $twenty_four now
+echo  $twenty_four 'success'
+sleep $sleepValue
+
+task $script conc JD_COOKIE $twenty_five now
+echo  $twenty_five 'success'
+sleep $sleepValue
+
+echo '执行结束'
